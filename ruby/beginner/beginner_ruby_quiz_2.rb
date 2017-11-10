4ff2e1c27576487ea04fe0b33a9da388bfde408c
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2017-11-09 16:03:15 -0800

    added solutions for beginner ruby quiz #2

---
+++ ruby/beginner/beginner_ruby_quiz_2.rb
@@ -0,0 +1,171 @@
+# my_array = ["matt", "phil"]  
+# my_array is an array with the values "matt" and "phil". Arrays are collections of values.
+# Create an array with the values 1 and 2.
+
+    [1, 2]
+
+# Create an array with nothing inside it.
+
+    []
+
+# Create an Array that contains the values "mike tyson", 89, and 9.7.
+
+    ["mike tyson", 89, 9.7]
+
+# Identify the components of the following expression:    
+# students = ["mary", "kathy", "frank"]
+
+    # students is the variable name, '=' is the assignment operator, and
+    # [] defines an array; the array is composed of three strings, "mary", "kathy", and "frank"
+
+# What does the following expression return:    
+# ["uno", "dos", "tres"].length()
+
+    3
+
+# Is this array valid?
+# name = "clem"
+# age = 32
+# [name, age]
+
+    # Yes
+
+# lyric = ["laughter", "it's", "free"]    
+# Get the first element of the lyric Array.
+
+    lyric.first # also lyric[0]
+
+# lyric = ["laughter", "it's", "free"]
+# Get the last element of the lyric Array.
+
+    lyric.last # also lyric[-1]
+
+# ruff = ["stop", "drop", "shut 'em down"]
+# Add the string "open up shop" to the end of the ruff Array.
+
+    ruff.push("open up shop")
+
+# What does the following expression evaluate to?
+# 5 == 5
+
+    true
+
+# What does the following expression evaluate to?
+# true = 6
+
+    "SyntaxError: can't assign to true"
+
+# Demonstrate that "matt" is the same as "matt".
+
+    "matt" == "matt"
+
+# What does the following expression evaluate to?
+# 4 != 4
+
+    false
+
+# What does the following expression evaluate to?
+# 5 > 4
+
+    true
+
+# What does the following expression print?
+# if 5 > 4
+#   puts "5 is greater than 4"
+# end
+  
+    "5 is greater than 4"
+
+# What does the following expression print?
+# if 5 < 4
+#   puts "5 is less than 4"
+# else
+#   puts "5 is not less than 4"
+# end
+
+    "5 is not less than 4"
+
+# What does the following expression print?
+
+# if "sam" == "cat"
+#   puts "sam equals cat"
+# elsif "matt" == "matt"
+#   puts "matt equals matt"
+# else
+#   puts "whatever"
+# end
+
+    "matt equals matt"
+
+# What does the following expression print?
+# if 5
+#   puts "Hello there"
+# end
+
+    "Hello there"
+
+# What does the following expression print?
+# if "bob"
+#   puts "bob is truthy"
+# else
+#   puts "bob is falsey"
+# end
+
+    "bob is truthy"
+
+# What does the following expression print?
+# if nil
+#   puts "nil is truthy"
+# else
+#   puts "nil is falsey"
+# end
+
+    "nil is falsey"
+
+# What does the following expression print?
+# puts "This syntax is cool" if true
+
+    "This syntax is cool"
+
+# What does the following expression print?
+# puts "Tall buildings" if false
+
+    #prints nothing; the boolean condition is not satisfied (i.e evaluates to true)
+
+# What is the value of x?
+# x = 5
+# x = x + 1
+
+    6
+
+# What is the value of mode?
+# mode = "chill"
+# mode += "hippie"
+
+    "chillhippie"
+
+# What does the following code print?
+# counter = 0
+# while counter < 3
+#   puts "Went through the loop"
+#   counter = counter + 1
+# end
+
+    "Went through the loop"
+    "Went through the loop"
+    "Went through the loop"
+
+# What does the following code print?
+# while 3 > 15
+#   puts "This is the end"
+# end
+
+    #It will print nothing because the condition does not evaluate to true
+
+# What does the following code print?
+# while true
+#   puts "This is the song that never ends"
+# end
+
+    #The program will enter an infinite loop, repeatedly printing
+    "This is the song that never ends"
